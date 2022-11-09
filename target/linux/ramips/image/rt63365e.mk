217c2df4ffbc5975779998e6986c0665c2d20be1
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2022-11-09 23:02:27 +0100

    ramips: Use the new tctrx tool to correctly pack Asus DSL-N14 firmware.

--- target/linux/ramips/image/rt63365e.mk
+++ target/linux/ramips/image/rt63365e.mk
@@ -7,8 +7,13 @@ define Device/asus_dsl-n14u
   DEVICE_MODEL := DSL-N14U
   SUPPORTED_DEVICES += dsl-n14u
   SOC := rt63365e
-  IMAGES := trx
-  IMAGE/trx := trx
-  IMAGE_SIZE := 16000k
+  IMAGES := DSL-N14U.trx
+  IMAGE/DSL-N14U.trx := tctrx
 endef
 TARGET_DEVICES += asus_dsl-n14u
+
+define Build/tctrx
+  cat "$(IMAGE_KERNEL)" "$(IMAGE_ROOTFS)" > "`dirname $@`/tclinux"
+	$(STAGING_DIR_HOST)/bin/tctrx -k `stat -c%s "$(IMAGE_KERNEL)"` -r `stat -c%s "$(IMAGE_ROOTFS)"` -f "`dirname $@`/tclinux" -o "`dirname $@`/tclinux.bin"
+  mv "`dirname $@`/tclinux.bin" $@
+endef
