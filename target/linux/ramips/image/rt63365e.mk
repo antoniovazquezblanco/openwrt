42d2bec3b14d50650fc86f0f36174de36eb54e7d
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2022-11-12 19:42:58 +0100

    ramips: Fix image generation for DSL-N14U

--- target/linux/ramips/image/rt63365e.mk
+++ target/linux/ramips/image/rt63365e.mk
@@ -8,12 +8,12 @@ define Device/asus_dsl-n14u
   SUPPORTED_DEVICES += dsl-n14u
   SOC := rt63365e
   IMAGES := DSL-N14U.trx
-  IMAGE/DSL-N14U.trx := tctrx
+  IMAGE/DSL-N14U.trx := append-kernel | lzma | append-rootfs | tctrx
 endef
 TARGET_DEVICES += asus_dsl-n14u
 
 define Build/tctrx
-  cat "$(IMAGE_KERNEL)" "$(IMAGE_ROOTFS)" > "`dirname $@`/tclinux"
+  mv $@ "`dirname $@`/tclinux"
 	$(STAGING_DIR_HOST)/bin/tctrx -k `stat -c%s "$(IMAGE_KERNEL)"` -r `stat -c%s "$(IMAGE_ROOTFS)"` -f "`dirname $@`/tclinux" -o "`dirname $@`/tclinux.bin"
   mv "`dirname $@`/tclinux.bin" $@
 endef
