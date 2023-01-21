e3bd40b47513bf4ec80c153f0c2ba32a73965311
Antonio Vázquez <antoniovazquezblanco@gmail.com>
2023-01-21 12:51:19 +0100

    rt63365e: Change acpu arch

--- target/linux/ramips/rt63365e/target.mk
+++ target/linux/ramips/rt63365e/target.mk
@@ -2,9 +2,10 @@
 # Copyright (C) 2022 OpenWrt.org
 #
 
+ARCH:=mips
 SUBTARGET:=rt63365e
-BOARDNAME:=RT63365E based boards
 CPU_TYPE:=34kc
+BOARDNAME:=RT63365E based boards
 
 define Target/Description
 	Build firmware images for Ralink RT63365E based boards.
